5efd9b7510e84b42a777f38534532f36ba91d072
kloness <pablog.acc@gmail.com>
2019-07-21 12:53:18 -0400

    models: Book and Category

--- techk/apps/base/models.py
+++ techk/apps/base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+
+class Book(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    thumbnail_url = models.URLField()
+    price = models.CharField(max_length=20)
+    stock = models.BooleanField()
+    product_description = models.TextField()
+    upc = models.CharField(max_length=50)
