3a264f778a97d81a89c94f1d9c7c38f4206b9b38
kloness <pablog.acc@gmail.com>
2019-07-21 14:22:32 -0400

    move scraping process to scraper logic

--- techk/apps/api/views.py
+++ techk/apps/api/views.py
@@ -1,11 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
-from timeit import default_timer as timer
 
 from apps.base import models
-from apps.base.models import reset_db
-from apps.scraper.logic import scrap_categories
+from apps.scraper.logic import scrap_process
 
 
 class Scraping(APIView):
@@ -13,11 +11,5 @@ class Scraping(APIView):
 
     def get(self, request):
         print('Scraping')
-        # delete books and categories for a fresh start
-        reset_db()
-        # scrap categories
-        start = timer()
-        scrap_categories()
-        end = timer()
-        print('scrap categories: ' + str((end - start)) + ' seconds')
-        return Response({}, status=status.HTTP_200_OK)
+        scrap_process()
+        return Response(status=status.HTTP_200_OK)
--- techk/apps/scraper/logic.py
+++ techk/apps/scraper/logic.py
@@ -3,8 +3,10 @@ from __future__ import unicode_literals
 
 from bs4 import BeautifulSoup
 import requests
+from timeit import default_timer as timer
 
 from apps.base import models
+from apps.base.models import reset_db
 
 BASE_URL = 'http://books.toscrape.com/'
 
@@ -14,9 +16,19 @@ def get_page(url):
     return BeautifulSoup(content, 'html.parser')
 
 
-def scrap_categories():
+def scrap_process():
+    # delete books and categories for a fresh start
+    reset_db()
     # get page object
     page = get_page(BASE_URL)
+    # scrap categories
+    start = timer()
+    scrap_categories(page)
+    end = timer()
+    print('scrap categories: ' + str((end - start)) + ' seconds')
+
+
+def scrap_categories(page):
     # get categories from the sidebar
     side_categories = page.select('.side_categories ul li ul li a')
     # save categories in database
